080d4f6c43549da7f810f952f7297ce34bb68428
Johannes <johannes.bluhm@outlook.com>
2022-06-15 13:37:22 +0200

    fix compile bug

--- Code/Display/Display.ino
+++ Code/Display/Display.ino
@@ -11,7 +11,9 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#include "res"
+#include "src/0.h"
+#include "src/1.h"
+
 
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
