9a5e7667622b2fdf07c02b3841d1404a7a2571e9
Johannes <johannes.bluhm@outlook.com>
2022-06-15 14:07:05 +0200

    added button

--- Code/Display/Display.ino
+++ Code/Display/Display.ino
@@ -11,6 +11,8 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+#define BTN_PIN 15
+
 #include "src/0.h"
 #include "src/1.h"
 
@@ -18,27 +20,48 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
+
+int lastButtonState;    // the previous state of button
+int currentButtonState; // the current state of button
+
+
 void setup() {
   Serial.begin(9600);
 
+  pinMode(BTN_PIN, INPUT);
+  currentButtonState = digitalRead(BTN_PIN);
+
+
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;); // Don't proceed, loop forever
   }
 }
 
 int currentImage = -1;
 int loudness = 0;
+bool draw = true;
 
 void loop() {
+  lastButtonState    = currentButtonState;      // save the last state
+  currentButtonState = digitalRead(BTN_PIN); // read new state
   int imageToDraw;
-  if (loudness > 127) {
-    imageToDraw = 1;
-  } else if (loudness > 63) {
-    imageToDraw = 1;
+
+  if (lastButtonState == HIGH && currentButtonState == LOW) {
+    draw = !draw;
+  }
+
+  if (draw) {
+    if (loudness > 127) {
+      imageToDraw = 1;
+    } else if (loudness > 63) {
+      imageToDraw = 1;
+    } else {
+      imageToDraw = 0;
+    }
   } else {
-    imageToDraw = 0;
+    imageToDraw = -1;
   }
 
   if (imageToDraw != currentImage) {
